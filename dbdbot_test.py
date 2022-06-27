529daf887e70fc34ee7c9e7814029b03508a3aa6
Pedro Militao <pedromilitao@denox.com.br>
2022-06-26 23:37:31 -0300

    descomentando testes

--- dbdbot_test.py
+++ dbdbot_test.py
@@ -30,105 +30,105 @@ async def test_verify_before_register(interface):
 
 
 
-# @test_collector()
-# async def test_assassino_unregistered(interface):
-#     message = await interface.send_message(
-#         "-assassino"
-#     )
-#     await interface.get_delayed_reply(quick_wait_time, interface.assert_message_contains, "você não está registrado no sistema, para se registrar utilize o comando -registro `(link para seu perfil steam)`")
+@test_collector()
+async def test_assassino_unregistered(interface):
+    message = await interface.send_message(
+        "-assassino"
+    )
+    await interface.get_delayed_reply(quick_wait_time, interface.assert_message_contains, "você não está registrado no sistema, para se registrar utilize o comando -registro `(link para seu perfil steam)`")
 
-# @test_collector()
-# async def test_survivor_unregistered(interface):
-#     message = await interface.send_message(
-#         "-sobrevivente"
-#     )
-#     await interface.get_delayed_reply(quick_wait_time, interface.assert_message_contains, "você não está registrado no sistema, para se registrar utilize o comando -registro `(link para seu perfil steam)`")
-
-
-# @test_collector()
-# async def test_assassino_invalid_link(interface):
-#     message = await interface.send_message(
-#         "-assassino link_invalido"
-#     )
-#     await interface.get_delayed_reply(quick_wait_time, interface.assert_message_contains, "por favor envie um url de perfil da steam Válido")
-
-# @test_collector()
-# async def test_geral_invalid_link(interface):
-#     message = await interface.send_message(
-#         "-geral link_invalido"
-#     )
-#     await interface.get_delayed_reply(quick_wait_time, interface.assert_message_contains, "por favor envie um url de perfil da steam Válido")
-
-# @test_collector()
-# async def test_survivor_invalid_link(interface):
-#     message = await interface.send_message(
-#         "-sobrevivente link_invalido"
-#     )
-#     await interface.get_delayed_reply(quick_wait_time, interface.assert_message_contains, "por favor envie um url de perfil da steam Válido")
-
-# @test_collector()
-# async def test_assassino_steamcommunity_invalid_link(interface):
-#     message = await interface.send_message(
-#         "-assassino steamcommunity.com"
-#     )
-#     await interface.get_delayed_reply(quick_wait_time, interface.assert_message_contains, "por favor envie um url de perfil da steam Válido")
-
-# @test_collector()
-# async def test_geral_steamcommunity_invalid_link(interface):
-#     message = await interface.send_message(
-#         "-geral steamcommunity.com"
-#     )
-#     await interface.get_delayed_reply(quick_wait_time, interface.assert_message_contains, "por favor envie um url de perfil da steam Válido")
-
-# @test_collector()
-# async def test_survivor_steamcommunity_invalid_link(interface):
-#     message = await interface.send_message(
-#         "-sobrevivente steamcommunity.com"
-#     )
-#     await interface.get_delayed_reply(quick_wait_time, interface.assert_message_contains, "por favor envie um url de perfil da steam Válido")
-
-# # test real link
-# @test_collector()
-# async def test_survivor_valid_link_title(interface):
-#     message = await interface.send_message(
-#         "-sobrevivente https://steamcommunity.com/id/Sabrelobo/"
-#     )
-#     embed = Embed(title=":Dwight: Sobrevivente Stats | Dead by Daylight - Brasil")
-#     await interface.get_delayed_reply(long_wait_time, interface.assert_embed_equals,embed, ["title"])
-
-# @test_collector()
-# async def test_killer_valid_link_title(interface):
-#     message = await interface.send_message(
-#         "-assassino https://steamcommunity.com/id/Sabrelobo/"
-#     )
-#     embed = Embed(title=":Clown: Assassino Stats | Dead by Daylight - Brasil")
-#     await interface.get_delayed_reply(long_wait_time, interface.assert_embed_equals,embed, ["title"])
+@test_collector()
+async def test_survivor_unregistered(interface):
+    message = await interface.send_message(
+        "-sobrevivente"
+    )
+    await interface.get_delayed_reply(quick_wait_time, interface.assert_message_contains, "você não está registrado no sistema, para se registrar utilize o comando -registro `(link para seu perfil steam)`")
 
 
-# @test_collector()
-# async def test_survivor_valid_link_description(interface):
-#     message = await interface.send_message(
-#         "-sobrevivente https://steamcommunity.com/id/Sabrelobo/"
-#     )
-#     embed = Embed(description="Estatísticas de Sobrevivente de **Sabrelobo** no Dead by Daylight.")
-#     await interface.get_delayed_reply(long_wait_time, interface.assert_embed_equals,embed, ["description"])
+@test_collector()
+async def test_assassino_invalid_link(interface):
+    message = await interface.send_message(
+        "-assassino link_invalido"
+    )
+    await interface.get_delayed_reply(quick_wait_time, interface.assert_message_contains, "por favor envie um url de perfil da steam Válido")
 
-# @test_collector()
-# async def test_killer_valid_link_description(interface):
-#     message = await interface.send_message(
-#         "-assassino https://steamcommunity.com/id/Sabrelobo/"
-#     )
-#     embed = Embed(description="Estatísticas de Assassino de **Sabrelobo** no Dead by Daylight.")
-#     await interface.get_delayed_reply(long_wait_time, interface.assert_embed_equals,embed, ["description"])
+@test_collector()
+async def test_geral_invalid_link(interface):
+    message = await interface.send_message(
+        "-geral link_invalido"
+    )
+    await interface.get_delayed_reply(quick_wait_time, interface.assert_message_contains, "por favor envie um url de perfil da steam Válido")
+
+@test_collector()
+async def test_survivor_invalid_link(interface):
+    message = await interface.send_message(
+        "-sobrevivente link_invalido"
+    )
+    await interface.get_delayed_reply(quick_wait_time, interface.assert_message_contains, "por favor envie um url de perfil da steam Válido")
+
+@test_collector()
+async def test_assassino_steamcommunity_invalid_link(interface):
+    message = await interface.send_message(
+        "-assassino steamcommunity.com"
+    )
+    await interface.get_delayed_reply(quick_wait_time, interface.assert_message_contains, "por favor envie um url de perfil da steam Válido")
+
+@test_collector()
+async def test_geral_steamcommunity_invalid_link(interface):
+    message = await interface.send_message(
+        "-geral steamcommunity.com"
+    )
+    await interface.get_delayed_reply(quick_wait_time, interface.assert_message_contains, "por favor envie um url de perfil da steam Válido")
+
+@test_collector()
+async def test_survivor_steamcommunity_invalid_link(interface):
+    message = await interface.send_message(
+        "-sobrevivente steamcommunity.com"
+    )
+    await interface.get_delayed_reply(quick_wait_time, interface.assert_message_contains, "por favor envie um url de perfil da steam Válido")
+
+# test real link
+@test_collector()
+async def test_survivor_valid_link_title(interface):
+    message = await interface.send_message(
+        "-sobrevivente https://steamcommunity.com/id/Sabrelobo/"
+    )
+    embed = Embed(title=":Dwight: Sobrevivente Stats | Dead by Daylight - Brasil")
+    await interface.get_delayed_reply(long_wait_time, interface.assert_embed_equals,embed, ["title"])
+
+@test_collector()
+async def test_killer_valid_link_title(interface):
+    message = await interface.send_message(
+        "-assassino https://steamcommunity.com/id/Sabrelobo/"
+    )
+    embed = Embed(title=":Clown: Assassino Stats | Dead by Daylight - Brasil")
+    await interface.get_delayed_reply(long_wait_time, interface.assert_embed_equals,embed, ["title"])
 
 
-# @test_collector()
-# async def test_survivor_reply_has_rank_image(interface):
-#     await interface.assert_reply_has_image("-sobrevivente https://steamcommunity.com/id/Sabrelobo/")
+@test_collector()
+async def test_survivor_valid_link_description(interface):
+    message = await interface.send_message(
+        "-sobrevivente https://steamcommunity.com/id/Sabrelobo/"
+    )
+    embed = Embed(description="Estatísticas de Sobrevivente de **Sabrelobo** no Dead by Daylight.")
+    await interface.get_delayed_reply(long_wait_time, interface.assert_embed_equals,embed, ["description"])
 
-# @test_collector()
-# async def test_killer_reply_has_rank_image(interface):
-#     await interface.assert_reply_has_image("-assassino https://steamcommunity.com/id/Sabrelobo/")
+@test_collector()
+async def test_killer_valid_link_description(interface):
+    message = await interface.send_message(
+        "-assassino https://steamcommunity.com/id/Sabrelobo/"
+    )
+    embed = Embed(description="Estatísticas de Assassino de **Sabrelobo** no Dead by Daylight.")
+    await interface.get_delayed_reply(long_wait_time, interface.assert_embed_equals,embed, ["description"])
+
+
+@test_collector()
+async def test_survivor_reply_has_rank_image(interface):
+    await interface.assert_reply_has_image("-sobrevivente https://steamcommunity.com/id/Sabrelobo/")
+
+@test_collector()
+async def test_killer_reply_has_rank_image(interface):
+    await interface.assert_reply_has_image("-assassino https://steamcommunity.com/id/Sabrelobo/")
 
 
 
